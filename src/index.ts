7907cdfa3ae8ea20e963c44b622440b86314ec57
Sergey Telpuk <sergey.telpuk@itrexgroup.com>
2019-09-10 15:49:27 +0300

    added request-filter

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from 'rbac.module';
 export * from 'exceptions/rbac.exceptions';
 export * from 'role/interfaces/role.interface';
 export * from 'role/role.rbac';
+export * from 'constans/index';
