408ee3e6d2f674cb1d5da8fdcc3ff38c4fb34afe
Sergey Telpuk <sergey.telpuk@itrexgroup.com>
2019-09-12 13:00:37 +0300

    fixed check codestyle #5

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 function __export(m) {
-  for (var p in m) if (!exports.hasOwnProperty(p)) {
-    exports[p] = m[p];
+  for (var p in m) {
+    if (!exports.hasOwnProperty(p)) {
+      exports[p] = m[p];
+    }
   }
 }
 exports.__esModule = true;
