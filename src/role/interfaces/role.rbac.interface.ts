a68d04693cc97fd5eb4d60277ff3e01941afbe31
Elton Lau <twilightlau94@gmail.com>
2021-05-01 00:43:33 +0800

    standardize the argument inputs

--- src/role/interfaces/role.rbac.interface.ts
+++ src/role/interfaces/role.rbac.interface.ts
@@ -1,6 +1,6 @@
 export interface IRoleRbac {
-  can(...permissions: string[]): boolean;
-  canAsync(...permissions: string[]): Promise<boolean>;
+  can(...permissions: string[]): boolean
+  canAsync(...permissions: string[]): Promise<boolean>
   any(permissions: string[][]): boolean
   anyAsync(permissions: string[][]): Promise<boolean>
 }
