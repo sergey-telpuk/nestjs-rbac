dad7c80d37a62252a9e6dacfce13e17b353006ed
Elton Lau <twilightlau94@gmail.com>
2021-05-01 08:05:00 +0800

    Unit testing on service & lint fixing

--- src/role/role.rbac.ts
+++ src/role/role.rbac.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common'
-import { IRoleRbac } from './interfaces/role.rbac.interface'
-import { IFilterPermission } from '../permissions/interfaces/filter.permission.interface'
-import { IParamsFilter } from '../params-filter/interfaces/params.filter.interface'
+import { Injectable } from '@nestjs/common';
+import { IRoleRbac } from './interfaces/role.rbac.interface';
+import { IFilterPermission } from '../permissions/interfaces/filter.permission.interface';
+import { IParamsFilter } from '../params-filter/interfaces/params.filter.interface';
 
 @Injectable()
 export class RoleRbac implements IRoleRbac {
@@ -13,44 +13,44 @@ export class RoleRbac implements IRoleRbac {
   ) {}
 
   async canAsync (...permissions: string[]): Promise<boolean> {
-    return this.checkPermissions<Promise<boolean>>(permissions, 'canAsync')
+    return this.checkPermissions<Promise<boolean>>(permissions, 'canAsync');
   }
 
   can (...permissions: string[]): boolean {
-    return this.checkPermissions<boolean>(permissions, 'can')
+    return this.checkPermissions<boolean>(permissions, 'can');
   }
 
   any (...permissions: string[][]): boolean {
     // loop through the list of permission list
     return (
       permissions
-        .map(async permission => {
+        .map(permission => {
           // check each of the permission list
-          return this.can(...permission)
+          return this.can(...permission);
         })
         // any permission list is true, then return true as result
         .some(result => result)
-    )
+    );
   }
 
   async anyAsync (...permissions: string[][]): Promise<boolean> {
     return (
       await Promise.all(
         permissions.map(async permission => {
-          return this.canAsync(...permission)
+          return await this.canAsync(...permission);
         }),
       )
-    ).some(result => result)
+    ).some(result => result);
   }
 
   private checkPermissions<T> (permissions, methodName): T {
     if (!permissions.length) {
-      return false as any
+      return false as any;
     }
     // check grant
     for (const permission of permissions) {
       if (!this.grant.includes(permission)) {
-        return false as any
+        return false as any;
       }
     }
 
@@ -58,14 +58,14 @@ export class RoleRbac implements IRoleRbac {
     for (const permission of permissions) {
       // check particular permission [permission@action]
       if (this.grant.includes(permission) && permission.includes('@')) {
-        const filter: string = permission.split('@')[1]
-        const filterService: IFilterPermission = this.filters[filter]
+        const filter: string = permission.split('@')[1];
+        const filterService: IFilterPermission = this.filters[filter];
         if (filterService) {
           return (
             filterService?.[methodName]?.(
               this.paramsFilter ? this.paramsFilter.getParam(filter) : null,
             ) ?? true
-          )
+          );
         }
       }
       // check particular permission [permission]
@@ -79,12 +79,12 @@ export class RoleRbac implements IRoleRbac {
               this.filters[filter]?.[methodName]?.(
                 this.paramsFilter ? this.paramsFilter.getParam(filter) : null,
               ) ?? true
-            )
+            );
           }
         }
       }
     }
 
-    return true as any
+    return true as any;
   }
 }
